caf9d55f673b1f41ff2f145be4b26da9ef691aaf
Charles Ross <chivalry@mac.com>
2017-03-17 15:15:31 -0700

    Try variable substitution

--- ubuntu-flask.bash
+++ ubuntu-flask.bash
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+proj="${1:myproject}"
+
 sudo apt-get update
 sudo apt-get -y install python3-pip python3-dev nginx
 sudo -H pip3 install --upgrade pip
 sudo -H pip3 install virtualenv
 
-mkdir ~/myproject
-cd ~/myproject
-virtualenv myprojectenv
-source myprojectenv/bin/activate
+mkdir ~/$proj
+cd ~/$proj
+virtualenv $proj
+source venv/bin/activate
 pip install gunicorn flask
 
-cat > ~/myproject/myproject.py <<EOL
+cat > ~/$proj/$proj.py <<EOL
 from flask import Flask
 app = Flask(__name__)
 
@@ -23,8 +25,8 @@ if __name__ == '__main__':
     app.run(host='0.0.0.0')
 EOL
 
-cat > ~/myproject/wsgi.py <<EOL
-from myproject import app
+cat > ~/$proj/wsgi.py <<EOL
+from ${proj} import app
 
 if __name__ == '__main__':
     app.run()
@@ -32,38 +34,38 @@ EOL
 
 deactivate
 
-sudo bash -c "cat > /etc/systemd/system/myproject.service" <<EOL
+sudo bash -c "cat > /etc/systemd/system/${proj}.service" <<EOL
 [Unit]
-Description=Gunicorn instance to serve myproject
+Description=Gunicorn instance to serve ${proj}
 After=network.target
 
 [Service]
 User=ubuntu
 Group=www-data
-WorkingDirectory=/home/ubuntu/myproject
-Environment="PATH=/home/ubuntu/myproject/myprojectenv/bin"
-ExecStart=/home/ubuntu/myproject/myprojectenv/bin/gunicorn --workers 3 --bind unix:myproject.sock -m 007 wsgi:app
+WorkingDirectory=/home/ubuntu/${proj}
+Environment="PATH=/home/ubuntu/${proj}/venv/bin"
+ExecStart=/home/ubuntu/${proj}/venv/bin/gunicorn --workers 3 --bind unix:${proj}.sock -m 007 wsgi:app
 
 [Install]
 WantedBy=multi-user.target
 EOL
 
-sudo systemctl start myproject
-sudo systemctl enable myproject
+sudo systemctl start $proj
+sudo systemctl enable $proj
 
-sudo bash -c "cat > /etc/nginx/sites-available/myproject" <<EOL
+sudo bash -c "cat > /etc/nginx/sites-available/${proj}" <<EOL
 server {
     listen 80;
     server_name server_domain_or_IP;
 
     location / {
         include proxy_params;
-        proxy_pass http://unix:/home/ubuntu/myproject/myproject.sock;
+        proxy_pass http://unix:/home/ubuntu/${proj}/${proj}.sock;
     }
 }
 EOL
 
-sudo ln -s /etc/nginx/sites-available/myproject /etc/nginx/sites-enabled
+sudo ln -s /etc/nginx/sites-available/$proj /etc/nginx/sites-enabled
 sudo nginx -t
 sudo rm /etc/nginx/sites-enabled/default
 sudo systemctl restart nginx
